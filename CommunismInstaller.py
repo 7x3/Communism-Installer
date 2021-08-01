f9d8cc4f195f768c14e889d86f07b75f48934e87
Joey Warrell <jjwarrell@gmail.com>
2021-07-31 21:06:38 -0400

    For Marx

---
+++ CommunismInstaller.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+import os
+import time
+
+print("Installing Communism")
+time.sleep(1)
+print("Writing Marx")
+time.sleep(1)
+print("Killing the bourgeosie")
+time.sleep(1)
+print("Putting Lenin in power")
+time.sleep(1)
+print("Worshipping Stalin")
+time.sleep(1)
+print("Freeing the proletariat")
+time.sleep(1)
+print("Supporting the Democrats")
+os.system("wget https://www.marxists.org/archive/marx/works/download/pdf/Manifesto.pdf")
+time.sleep(1)
+print("Opening Communist Manifesto")
+os.system("open Manifesto.pdf")
